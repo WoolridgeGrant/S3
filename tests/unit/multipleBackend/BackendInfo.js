8cca31407667e5c087ec0688be93b48efed39c94
Nicolas Humbert <nicolas.humbert@live.fr>
2017-03-23 18:00:54 -0700

    FIX: return invalid location constraint error

--- tests/unit/multipleBackend/BackendInfo.js
+++ tests/unit/multipleBackend/BackendInfo.js
@@ -7,26 +7,28 @@ const log = new DummyRequestLogger();
 const dummyBackendInfo = new BackendInfo('mem', 'file', '127.0.0.1');
 
 describe('BackendInfo class', () => {
-    describe('isValidControllingBackendParam', () => {
+    describe('controllingBackendParam', () => {
         it('should return false if objectLocationConstraint is invalid', () => {
-            const res = BackendInfo.isValidControllingBackendParam(
+            const res = BackendInfo.controllingBackendParam(
                 'notValid', 'file', '127.0.0.1', log);
-            assert.equal(res, false);
+            assert.equal(res.isValid, false);
+            assert.equal(res.locationContraintValue, 'object');
         });
         it('should return false if bucketLocationConstraint is invalid', () => {
-            const res = BackendInfo.isValidControllingBackendParam(
+            const res = BackendInfo.controllingBackendParam(
                 'mem', 'notValid', '127.0.0.1', log);
-            assert.equal(res, false);
+            assert.equal(res.isValid, false);
+            assert.equal(res.locationContraintValue, 'bucket');
         });
         it('should return false if requestEndpoint is invalid', () => {
-            const res = BackendInfo.isValidControllingBackendParam(
+            const res = BackendInfo.controllingBackendParam(
                 'mem', 'file', 'notValid', log);
-            assert.equal(res, false);
+            assert.equal(res.locationContraintValue, 'endpoint');
         });
         it('should return true if all backend parameters are valid', () => {
-            const res = BackendInfo.isValidControllingBackendParam(
+            const res = BackendInfo.controllingBackendParam(
                 'mem', 'file', '127.0.0.1', log);
-            assert.equal(res, true);
+            assert.equal(res.isValid, true);
         });
     });
     describe('getControllingLocationConstraint', () => {
